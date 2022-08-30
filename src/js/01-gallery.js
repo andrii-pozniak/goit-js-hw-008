7271f786985a104b8dd1334a1211272d0df6e6a4
Andrii Pozniak <andriipozniak@gmail.com>
2022-08-30 21:22:41 +0300

    add structure

---
+++ src/js/01-gallery.js
@@ -0,0 +1,30 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox";
+
+const imgGalleryCard = document.querySelector(`.gallery`);
+const imgGallery = onCreateImgCard(galleryItems);
+
+// Create card
+imgGalleryCard.insertAdjacentHTML("beforeend", imgGallery);
+
+function onCreateImgCard(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+       return `
+       <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+          />
+     </a>
+    </li>
+       ` ;       
+    }).join("");
+    
+};
+
+const light = new SimpleLightbox(`.gallery__item a`, { captionsData: 'alt',captionDelay: 250,});
+console.log(galleryItems);
