8737118f5e70b84bef56dd3e326df91e90ef8166
Andrii Pozniak <andriipozniak@gmail.com>
2022-09-02 23:18:21 +0300

    add change  2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from "@vimeo/player";
 
+let throttle = require('lodash.throttle');
+
+const onSeeTime = document.querySelector(`#vimeo-player`);
+
+// const onTimePlay = 0;
+
+ const iframe = document.querySelector('iframe');
+        const player = new Player(iframe);
+
+        player.on(`timeupdate`, throttle(onTimePlay, 1000));
+
+        function onTimePlay(timeupdate) {
+
+            const run = timeupdate.seconds;
+            const onRun = localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
+
+            console.log(onRun)
+        };
+
+        function onStartPlay(timeupdate) {
+            
+        }
