42358c679a9e186487bde4f1bb58f1a228f31a8b
Andrii Pozniak <andriipozniak@gmail.com>
2022-09-03 12:03:49 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,6 @@ let throttle = require('lodash.throttle');
 
 const onSeeTime = document.querySelector(`#vimeo-player`);
 
-// const onTimePlay = 0;
 
  const iframe = document.querySelector('iframe');
         const player = new Player(iframe);
@@ -13,42 +12,10 @@ const onSeeTime = document.querySelector(`#vimeo-player`);
 
         function onTimePlay(timeupdate) {
             console.log(timeupdate)
-            // timeupdate.preventDefault();
-            const run = timeupdate.seconds;
-            const onRun = localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
-
-            console.log(onRun)
-
-            player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
-               const onBeginPlay = localStorage.getItem("videoplayer-current-time")
-                seconds = onBeginPlay;
-            }).catch(function(error) {
-                switch (error.name) {
-                    case 'RangeError':
-                        // the time was less than 0 or greater than the video’s duration
-                        break;
+          
             
-                    default:
-                        // some other error occurred
-                        break;
-                }
-            });
-        };
-
-        // player.setCurrentTime(onRun).then(function(seconds) {
-        //     seconds = onRun;
-        // }).catch(function(error) {
-        //     switch (error.name) {
-        //         case 'RangeError':
-        //             // the time was less than 0 or greater than the video’s duration
-        //             break;
-        
-        //         default:
-        //             // some other error occurred
-        //             break;
-        //     }
-        // });
-
-        // function onStartPlay(timeupdate) {
-            
-        // }
+             localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
+                   
+        }
+       
+        player.setCurrentTime(localStorage.getItem("videoplayer-current-time")); 
