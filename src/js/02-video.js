43844262f64a33396c2640d8242d810954f59745
Andrii Pozniak <andriipozniak@gmail.com>
2022-09-06 22:24:38 +0300

    change task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,6 +2,7 @@ import Player from "@vimeo/player";
 
 let throttle = require('lodash.throttle');
 
+const STORAGE_KEY = "videoplayer-current-time"
 const onSeeTime = document.querySelector(`#vimeo-player`);
 
 
@@ -13,8 +14,13 @@ const onSeeTime = document.querySelector(`#vimeo-player`);
         function onTimePlay(timeupdate) {
             console.log(timeupdate)          
             
-             localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
+             localStorage.setItem(STORAGE_KEY, timeupdate.seconds);
                    
         }
+      
+        const saveMassage = localStorage.getItem(STORAGE_KEY);
+        if (saveMassage) {
+            player.setCurrentTime(saveMassage); 
+        }
        
-        player.setCurrentTime(localStorage.getItem("videoplayer-current-time")); 
+        
